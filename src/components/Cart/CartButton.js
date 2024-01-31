ba7cb3e63e3d98a91cc551627d8e1c36ef9f752b
Abdumalik <abdumaliksharipov@gmail.com>
2024-01-31 09:02:19 -0600

    feat: add handle show cart slice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { uiActions } from '../../store/ui-slice.js';
 import classes from './CartButton.module.css';
 
+import { useDispatch } from 'react-redux';
+
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
