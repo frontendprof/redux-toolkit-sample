0c218c4bdfa6d441d6fdd398c1069bd62a6efa0c
Abdumalik <abdumaliksharipov@gmail.com>
2024-02-01 06:48:30 -0600

    feat: add http request to firebase backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -6,6 +7,18 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      'https://redux-toolkit-909a7-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
